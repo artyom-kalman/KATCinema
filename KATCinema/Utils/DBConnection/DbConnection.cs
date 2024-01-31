5aa3d4642cbd54540a8897e0ea58ffc8aa71b583
Артём Калашников <kalashnikov252500@gmail.com>
2024-01-31 11:58:58 +1000

    Add DB Connection

---
+++ KATCinema/Utils/DBConnection/DbConnection.cs
@@ -0,0 +1,30 @@
+﻿using Npgsql;
+using System.Security.Cryptography.X509Certificates;
+
+class DBConnection
+{
+    public NpgsqlConnection Connect(string connString)
+    {
+        var conn = new NpgsqlConnection(connString);
+        conn.ProvideClientCertificatesCallback += new ProvideClientCertificatesCallback(MyClientCertificates);
+
+        try
+        {
+            conn.Open();
+        }
+        catch(Exception ex)
+        {
+            throw(new Exception($"Connection error: {ex.Message}"));
+        }
+        
+        Console.WriteLine("Connection is succesfully established");
+
+        return conn;
+    }
+
+    private void MyClientCertificates(X509CertificateCollection certificates)
+    {
+        var cert = new X509Certificate("Utils\\DBConnection\\root.crt");
+        certificates.Add(cert);
+    }
+}
