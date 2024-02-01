19145974434fe7e14077be71d7cd8041fa254956
Артём Калашников <kalashnikov252500@gmail.com>
2024-02-01 16:02:59 +1000

    Add MovieController

---
+++ KATCinema/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using KATCinema.Models;
+using KATCinema.Utils.DBConnection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KATCinema.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Movie> movies = _context.Movies.ToList();
+            return View(movies);
+        }
+    }
+}
