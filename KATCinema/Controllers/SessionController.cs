268b302685d7f2e9852d2b9e556f0671c00e2130
ArtLop <a.lopatien@ya.ru>
2024-02-04 19:11:32 +1000

    Add Booking check

--- KATCinema/Controllers/SessionController.cs
+++ KATCinema/Controllers/SessionController.cs
@@ -19,10 +19,30 @@ namespace KATCinema.Controllers
             List<Session> sessions = _context.Sessions.Include(session => session.Movie).ToList();
             return View(sessions);
         }
+        [HttpGet]
         public IActionResult Booking(int id)
         {
             Session sesion = _context.Sessions.Include(session => session.Hall).FirstOrDefault(x => x.Id == id);
             return View(sesion);
+        }
+        [HttpPost]
+        public IActionResult Booking(int id, bool[] seat)
+        {
+            string seatChecked = "";
+            Session sesion = _context.Sessions.Include(session => session.Hall).FirstOrDefault(x => x.Id == id);
+            int seatInRow = sesion.Hall.TotalSeats / sesion.Hall.TotalRows;
+            for (int i = 1;i<=sesion.Hall.TotalRows; i++)
+            {
+                for (int j = 1; j <= sesion.Hall.TotalRows; j++)
+                {
+                    string qe = Request.Form[$"{i} {j}"];
+                    if (Request.Form[$"{i} {j}"] == "true")
+                    {
+                        seatChecked += $"Ряд:{i} | Место {j}\n";
+                    }
+                }
+            }
+            return Content(seatChecked);
         }
     }
 }
