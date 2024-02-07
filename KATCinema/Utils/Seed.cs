2ad2edd3162647d21749caed1833aca70359b084
Артём Калашников <kalashnikov252500@gmail.com>
2024-02-07 12:04:22 +1000

    Add user and roles seeding

--- KATCinema/Utils/Seed.cs
+++ KATCinema/Utils/Seed.cs
@@ -1,68 +1,56 @@
-﻿//using KATCinema.Utils;
-//using KATCinema.Utils.DBConnection;
-//using Microsoft.AspNetCore.Identity;
-//using KATCinema.Models;
+﻿using KATCinema.Utils;
+using KATCinema.Utils.DBConnection;
+using Microsoft.AspNetCore.Identity;
+using KATCinema.Models;
 
-//namespace RunGroopWebApp.Data
-//{
-//    public class Seed
-//    {
-//        public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
-//        {
-//            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
-//            {
-//                //Roles
-//                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+namespace KATCinema.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
+        {
+            var serviceScope = applicationBuilder.ApplicationServices.CreateScope();
 
-//                if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
-//                    await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
-//                if (!await roleManager.RoleExistsAsync(UserRoles.User))
-//                    await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+            // Roles
+            var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-//                //Users
-//                var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<User>>();
-//                string adminUserEmail = "teddysmithdeveloper@gmail.com";
+            if (!await roleManager.RoleExistsAsync(UserRoles.Admin.ToString()))
+                await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin.ToString()));
+            if (!await roleManager.RoleExistsAsync(UserRoles.Basic.ToString()))
+                await roleManager.CreateAsync(new IdentityRole(UserRoles.Basic.ToString()));
 
-//                var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
-//                if (adminUser == null)
-//                {
-//                    var newAdminUser = new User()
-//                    {
-//                        UserName = "teddysmithdev",
-//                        Email = adminUserEmail,
-//                        EmailConfirmed = true,
-//                        Address = new Address()
-//                        {
-//                            Street = "123 Main St",
-//                            City = "Charlotte",
-//                            State = "NC"
-//                        }
-//                    };
-//                    await userManager.CreateAsync(newAdminUser, "Coding@1234?");
-//                    await userManager.AddToRoleAsync(newAdminUser, UserRoles.Admin);
-//                }
+            // Admin user
+            var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<User>>();
+            string adminUserEmail = "admin@mail.ru";
 
-//                string UserEmail = "user@etickets.com";
+            var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
+            if (adminUser == null)
+            {
+                var newAdminUser = new User()
+                {
+                    UserName = "kalman",
+                    Email = adminUserEmail,
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(newAdminUser, "1234");
+                await userManager.AddToRoleAsync(newAdminUser, UserRoles.Admin.ToString());
+            }
 
-//                var User = await userManager.FindByEmailAsync(UserEmail);
-//                if (User == null)
-//                {
-//                    var newUser = new User()
-//                    {
-//                        UserName = "app-user",
-//                        Email = UserEmail,
-//                        EmailConfirmed = true,
-//                        Address = new Address()
-//                        {
-//                            Street = "123 Main St",
-//                            City = "Charlotte",
-//                            State = "NC"
-//                        }
-//                    };
-//                    await userManager.CreateAsync(newUser, "Coding@1234?");
-//                    await userManager.AddToRoleAsync(newUser, UserRoles.User);
-//                }
-//            }
-//        }
-//    }
-//}
+            // Basic user
+            string UserEmail = "basic-user@mail.ru";
+
+            var User = await userManager.FindByEmailAsync(UserEmail);
+            if (User == null)
+            {
+                var newUser = new User()
+                {
+                    UserName = "basic-user",
+                    Email = UserEmail,
+                    EmailConfirmed = true,
+                };
+                await userManager.CreateAsync(newUser, "1234");
+                await userManager.AddToRoleAsync(newUser, UserRoles.Basic.ToString());
+            }
+        }
+    }
+}
