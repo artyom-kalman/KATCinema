bcd1355a6b32ca9077911dfd3c9662766a4146a7
Артём Калашников <kalashnikov252500@gmail.com>
2024-02-08 16:13:56 +1000

    Add models

---
+++ KATCinema/Models/Seat.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KATCinema.Models
+{
+    public class Seat
+    {
+        public int Id { get; set; }
+        [ForeignKey("Row")]
+        public int RowId { get; set; }
+        public Row Row { get; set; }
+
+    }
+}
