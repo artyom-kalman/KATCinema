1f105ee8395dddd0f4a550b984c7213be1dfb2ba
Артём Калашников <kalashnikov252500@gmail.com>
2024-02-08 18:51:04 +1000

    Update ViewModels

--- KATCinema/ViewModels/RegisterViewModel.cs
+++ KATCinema/ViewModels/RegisterViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 
 namespace KATCinema.ViewModels
 {
@@ -6,15 +7,18 @@ namespace KATCinema.ViewModels
     {
         [Display(Name = "Адрес электронной почты")]
         [Required(ErrorMessage = "Введите адрес почты")]
+        [EmailAddress]
         public string EmailAddress { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Введите пароль")]
         [Display(Name = "Пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
         [Display(Name = "Повторите пароль")]
         [Required(ErrorMessage = "Подтвердите пароль")]
         [DataType(DataType.Password)]
-        [Compare("Password",ErrorMessage = "Пароли не совпадают")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         public string ConfirmPassword { get; set; }
     }
 }
