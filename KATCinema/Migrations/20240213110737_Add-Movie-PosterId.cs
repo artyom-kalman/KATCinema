61fa73d1f410d2a96cacccf0bfd607931eca5d36
kalman <kalashnikov252500@gmail.com>
2024-02-13 21:08:05 +1000

    New migration

---
+++ KATCinema/Migrations/20240213110737_Add-Movie-PosterId.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KATCinema.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoviePosterId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Poster",
+                table: "Movies",
+                newName: "PosterUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PosterId",
+                table: "Movies",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PosterId",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "PosterUrl",
+                table: "Movies",
+                newName: "Poster");
+        }
+    }
+}
