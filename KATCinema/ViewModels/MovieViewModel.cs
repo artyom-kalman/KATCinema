c62f4631326c8a05f195731556fb6bfa5e054a7d
kalman <kalashnikov252500@gmail.com>
2024-02-13 18:27:55 +1000

    Add MovieViewModel

---
+++ KATCinema/ViewModels/MovieViewModel.cs
@@ -0,0 +1,12 @@
+﻿using KATCinema.Models;
+
+namespace KATCinema.ViewModels
+{
+    public class MovieViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public IFormFile Poster { get; set; }
+    }
+}
