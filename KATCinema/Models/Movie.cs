3522f85e6148d660503daf5713807c0eae0476a7
kalman <kalashnikov252500@gmail.com>
2024-02-14 13:37:38 +1000

    Update models: add display attribute

--- KATCinema/Models/Movie.cs
+++ KATCinema/Models/Movie.cs
@@ -7,11 +7,14 @@ namespace KATCinema.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Название")]
         public string Title { get; set; }
+        [Display(Name = "Описание")]
         public string Description { get; set; }
+        [Display(Name = "Длительность")]
         public int Duration { get; set; }
         public string PosterUrl { get; set; }
         public string PosterId { get; set; }
-        public List<Session> Sessions { get; set; }
+        public List<Session>? Sessions { get; set; }
     }
 }
--- KATCinema/ViewModels/MovieViewModel.cs
+++ KATCinema/ViewModels/MovieViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using KATCinema.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace KATCinema.ViewModels
 {
     public class MovieViewModel
     {
+        [Display(Name = "Название")]
         public string Title { get; set; }
+        [Display(Name = "Описание")]
         public string Description { get; set; }
+        [Display(Name = "Длительность")]
         public int Duration { get; set; }
+        [Display(Name = "Постер")]
         public IFormFile Poster { get; set; }
     }
 }
