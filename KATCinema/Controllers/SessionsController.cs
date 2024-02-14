9e6c0fbbf2683af8abb40c3c854cf7577b343ac3
kalman <kalashnikov252500@gmail.com>
2024-02-14 11:13:48 +1000

    Add sessions controller

---
+++ KATCinema/Controllers/SessionsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KATCinema.Models;
+using KATCinema.Utils.DBConnection;
+
+namespace KATCinema.Controllers
+{
+    public class SessionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SessionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sessions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Sessions.Include(s => s.Hall).Include(s => s.Movie);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Sessions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .Include(s => s.Hall)
+                .Include(s => s.Movie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        // GET: Sessions/Create
+        public IActionResult Create()
+        {
+            ViewData["HallId"] = new SelectList(_context.Halls, "Id", "Id");
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id");
+            return View();
+        }
+
+        // POST: Sessions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MovieId,StartTime,HallId,TicketPrice")] Session session)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(session);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HallId"] = new SelectList(_context.Halls, "Id", "Id", session.HallId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", session.MovieId);
+            return View(session);
+        }
+
+        // GET: Sessions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            ViewData["HallId"] = new SelectList(_context.Halls, "Id", "Id", session.HallId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", session.MovieId);
+            return View(session);
+        }
+
+        // POST: Sessions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MovieId,StartTime,HallId,TicketPrice")] Session session)
+        {
+            if (id != session.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(session);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SessionExists(session.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HallId"] = new SelectList(_context.Halls, "Id", "Id", session.HallId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", session.MovieId);
+            return View(session);
+        }
+
+        // GET: Sessions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .Include(s => s.Hall)
+                .Include(s => s.Movie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        // POST: Sessions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session != null)
+            {
+                _context.Sessions.Remove(session);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SessionExists(int id)
+        {
+            return _context.Sessions.Any(e => e.Id == id);
+        }
+    }
+}
