1741fd35177e2d1998c997555f12051318bc9cc2
kalman <kalashnikov252500@gmail.com>
2024-02-14 15:49:42 +1000

    Update CustomAuthorizationAttribute: Add role check

--- KATCinema/Utils/CustomAuthorizationAttribute.cs
+++ KATCinema/Utils/CustomAuthorizationAttribute.cs
@@ -6,6 +6,18 @@ namespace KATCinema.Utils
 {
     public class CustomAuthorizationFilterAttribute : Attribute, IAuthorizationFilter
     {
+        public string Role { get; set; }
+
+        public CustomAuthorizationFilterAttribute()
+        {
+            
+        }
+
+        public CustomAuthorizationFilterAttribute(string roles)
+        {
+            Role = roles;
+        }
+
         public void OnAuthorization(AuthorizationFilterContext context)
         {
             if (!IsAuthorized(context.HttpContext.User))
@@ -15,9 +27,24 @@ namespace KATCinema.Utils
                     { "controller", "Account" },  
                     { "action", "Login" }
                 });
+                return;
+            }
+
+            if (Role != null)
+            {
+                if (!IsInRole(context.HttpContext.User))
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary
+                    {
+                        { "controller", "Home" },
+                        { "action", "Index" }
+                    });
+                }
             }
         }
 
         private bool IsAuthorized(ClaimsPrincipal user) => user.Identity.IsAuthenticated;
+
+        private bool IsInRole(ClaimsPrincipal user) => user.IsInRole(Role);
     }
 }
